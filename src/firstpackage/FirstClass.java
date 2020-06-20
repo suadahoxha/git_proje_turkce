5ba24e63f181c8cf910ad29551fd3c85c911f301
suadahoxha <apple@apple-MacBook-Pro.local>
2020-06-20 14:35:37 -0400

    ikinci commit from local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,8 +3,20 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		System.out.println("first commit from local repository");
+		System.out.println("secand commit from local repository");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
