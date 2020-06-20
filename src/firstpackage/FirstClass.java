f0dece1df49148ce484e4dc82e02760b60d65daf
suadahoxha <apple@apple-MacBook-Pro.local>
2020-06-20 14:44:08 -0400

    third commit from local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -7,13 +7,43 @@ public class FirstClass {
 		
 		System.out.println("first commit from local repository");
 		System.out.println("secand commit from local repository");
+		System.out.println("third commit from local repository");
 		
 		
 		
-		
-		
-		
-		
+//		
+//		public static void main(String[] args){ line(satir 3)
+//		     sout
+//		}
+//		hangi versionu gondermek istiyorsam onu commit ediyorum ve
+//		kodumu geri remote gondeririyorum
+//		Command Prompt baslica komutlar(basic commands)
+//		pwd => Print Working directory => hali hazirdaki adresi goster
+//		ls => listele her hangi bir dosyanin icerigini listele
+//		cd =>change directory => konum degistir
+//		cd .. => bir hamle geri goturuyor
+//		Butun adimlar:
+//		cd + project path => properties tum adresi aldim
+//		git init => ve localda projemin icine git repository olusturduk
+//		eclipse ten alip codlarimizi staging area ya tasiyoruz
+//		git add src/firstpackage/FirstClass.java
+//		Ne zaman durumu yoklamak isterseniz bu komutu kullanabilirsiniz
+//		git status
+//		git add . => tum file ve folder lari staging area ya ekle demektir
+//		git commit -m "first commit from local repo" artik staging area dan local repository e aktarmis oluyoruz
+//		C:\Users\sam\eclipse-workspace\git_proje_turkce>git commit -m "first commit from local repo"
+//		[master (root-commit) e23d4ec] first commit from local repo
+//		 5 files changed, 49 insertions(+)
+//		 create mode 100644 .classpath
+//		 create mode 100644 .project
+//		 create mode 100644 .settings/org.eclipse.jdt.core.prefs
+//		 create mode 100644 bin/firstpackage/FirstClass.class
+//		 create mode 100644 src/firstpackage/FirstClass.java
+//		 git remote add origin https://github.com/HalilIbrahimHasan/git_proje_turkce.git tek seferlik remote repositoryi belirlemek icin bu command (komutu) kullaniyoruz.
+//		 Son olarak:
+//		 git push -u origin master => ilk ve son kez bu komutu kullaniyoruz
+//		
+//		
 		
 		
 		
