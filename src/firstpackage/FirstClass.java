5ffc8feab1112ade77917afaeffcff8b36019ebb
suadahoxha <apple@apple-MacBook-Pro.local>
2020-06-20 15:30:20 -0400

    confict olusturma

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -7,7 +7,7 @@ public class FirstClass {
 		
 		System.out.println("first commit from local repository");
 		System.out.println("secand commit from local repository");
-		System.out.println("third commit from local repository");
+		System.out.println("ucuncu commitimiz local repository denedi");
 		System.out.println("first commit from remote repository");
 		
 		
